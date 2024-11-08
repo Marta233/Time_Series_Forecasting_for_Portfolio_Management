83943108577d63b8779ee4c166eb6c177bc7fc31
Marta233 <martaassefa23@gmail.com>
2024-11-08 23:56:28 +0300

    merge Task_1

--- script/Data_Extract.py
+++ script/Data_Extract.py
@@ -31,9 +31,29 @@ class FinancialDataAnalysis:
         for ticker, df in self.data.items():
             # Reset the index so 'Date' becomes a column
             df.reset_index(inplace=True)  # Ensure 'Date' is a column, not the index
-            # Ensure columns are correctly named and structured
-            df.columns = ['Date', 'Adj Close', 'Close', 'High', 'Low', 'Open', 'Volume']
+            
             # Save cleaned data to a CSV file
             df.to_csv(f"{directory}/{ticker}.csv", index=False)
 
         print(f"Data saved to directory: {directory}")
+    
+    def save_to_merged_file(self, directory="financial_data"):
+        """
+        Merges all tickers' data into one DataFrame and saves it as a single CSV file.
+        """
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        merged_data = pd.DataFrame()  # Create an empty DataFrame to store the merged data
+
+        # Loop through each ticker's data and add the 'Ticker' column
+        for ticker, df in self.data.items():
+            df.reset_index(inplace=True)  # Ensure 'Date' is a column, not the index
+            df['Ticker'] = ticker  # Add a new column for the ticker name
+            # Ensure columns are correctly named and structured
+            df.columns = ['Date', 'Adj Close', 'Close', 'High', 'Low', 'Open', 'Volume','Ticker']
+            merged_data = pd.concat([merged_data, df], ignore_index=True)  # Merge the data
+            
+        # Save the merged data to a CSV file
+        merged_data.to_csv(f"{directory}/merged_financial_data.csv", index=False)
+        print(f"Data merged and saved to {directory}/merged_financial_data.csv")
